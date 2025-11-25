7483f6e2c10baa1720105e6f6a4c0b9ffa915407
hifzulaqliwork <hifzulaqli.work@gmail.com>
2025-11-25 13:37:18 +0700

    Update Blog_model.php

--- application/models/Blog_model.php
+++ application/models/Blog_model.php
@@ -30,12 +30,37 @@ class Blog_model extends CI_Model {
     public function insert($data)
     {
         $data['created_at'] = isset($data['created_at']) ? $data['created_at'] : date('Y-m-d H:i:s');
+
+        // generate slug if not provided
+        if (empty($data['slug']) && !empty($data['title'])) {
+            $data['slug'] = $this->create_unique_slug($data['title']);
+        } elseif (!empty($data['slug'])) {
+            // sanitize provided slug and ensure uniqueness
+            $this->load->helper('url');
+            $data['slug'] = url_title($data['slug'], 'dash', true);
+            if ($this->slug_exists($data['slug'])) {
+                $data['slug'] = $this->create_unique_slug($data['slug']);
+            }
+        }
+
         $this->db->insert('blogs', $data);
         return $this->db->insert_id();
     }
 
     public function update($id, $data)
     {
+        // If slug not provided but title is updated, regenerate unique slug
+        if (empty($data['slug']) && !empty($data['title'])) {
+            $data['slug'] = $this->create_unique_slug($data['title'], $id);
+        } elseif (!empty($data['slug'])) {
+            // sanitize provided slug and ensure uniqueness (exclude current id)
+            $this->load->helper('url');
+            $data['slug'] = url_title($data['slug'], 'dash', true);
+            if ($this->slug_exists($data['slug'], $id)) {
+                $data['slug'] = $this->create_unique_slug($data['slug'], $id);
+            }
+        }
+
         $this->db->where('id', (int)$id);
         return $this->db->update('blogs', $data);
     }
@@ -44,6 +69,19 @@ class Blog_model extends CI_Model {
     {
         $this->db->where('id', (int)$id);
         return $this->db->delete('blogs');
+    }
+
+    public function create_unique_slug($text, $exclude_id = null)
+    {
+        $this->load->helper('url');
+        $slug = url_title($text, 'dash', true);
+        $base = $slug;
+        $i = 0;
+        while ($this->slug_exists($slug, $exclude_id)) {
+            $i++;
+            $slug = $base . '-' . $i;
+        }
+        return $slug;
     }
 
     public function slug_exists($slug, $exclude_id = NULL)
